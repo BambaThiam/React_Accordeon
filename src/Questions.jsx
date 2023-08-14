f5763c75ad674c1c111d7107f7dcbf1f2150eb43
DayOneAI <bambathiam88@gmail.com>
2023-08-15 00:35:20 +0200

    End

--- src/Questions.jsx
+++ src/Questions.jsx
@@ -3,6 +3,8 @@ import SingleQuestion from './SingleQuestion'
 const Questions = ({ questions }) => {
   return (
     <section className="container">
+      {/* Title à supprimer si besoin */}
+      <h1 className="titleBamba">"BAMBA - React Accordion component"</h1>
       <h1>Questions</h1>
       {questions.map((question) => {
         return <SingleQuestion key={question.id} {...question} />
